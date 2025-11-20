f4e0b47f1e8a6613e7f9c83b7ff20a61d17adf67
engineerie <146146421+engineerie@users.noreply.github.com>
2025-11-20 20:35:13 +0000

    Deploying to gh-pages from @ engineerie/Svampskog@68ad9e1c3873043d65f46ff9a9ca703b8b2d3cc1 🚀

---
+++ _nuxt/XWxMjEY3.js
@@ -0,0 +1,1 @@
+import{r as t}from"./C9ns0zk3.js";import a from"./CsxoCgPn.js";import{z as e,N as n,e as s,w as c,P as i}from"#entry";import"./iik6CYzq.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(a,{color:"error",icon:i(o).ui.icons.caution},{default:c(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
